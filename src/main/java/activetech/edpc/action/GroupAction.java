12fe41ac8e9b530603dc092c9ce47e1a762aee1c
chenlp <chenlp@activetech.com.cn>
2023-03-15 14:18:38 +0800

    changes

--- src/main/java/activetech/edpc/action/GroupAction.java
+++ src/main/java/activetech/edpc/action/GroupAction.java
@@ -1,8 +1,17 @@
 package activetech.edpc.action;
 
+import activetech.base.pojo.dto.ActiveUser;
+import activetech.base.pojo.dto.PageQuery;
+import activetech.base.process.result.DataGridResultInfo;
+import activetech.edpc.pojo.dto.HspDbzlBasCustom;
+import activetech.edpc.pojo.dto.HspGrpInfQueryDto;
+import activetech.edpc.pojo.dto.QueryDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * @author ChenH
@@ -11,9 +20,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/group")
 public class GroupAction {
 
-    @RequestMapping("/togroup")
+    @RequestMapping("/querygroup")
     public String tohuzdtob(Model model, String moduleid) throws Exception {
         model.addAttribute("moduleid", moduleid);
         return "/edpc/group/querygroup";
+    }
+
+    @RequestMapping("/querygroup_result")
+    @ResponseBody
+    public DataGridResultInfo queryGroupResult(HspGrpInfQueryDto hspGrpInfQueryDto, ActiveUser activeUser){
+        // TODO 分页查询群组列表
+
+        DataGridResultInfo dataGridResultInfo = new DataGridResultInfo();
+        // 填充total
+//        dataGridResultInfo.setTotal(total);
+        // 填充rows
+//        dataGridResultInfo.setRows(list);
+        return dataGridResultInfo;
     }
 }
--- src/main/java/activetech/edpc/pojo/dto/HspGrpInfQueryDto.java
+++ src/main/java/activetech/edpc/pojo/dto/HspGrpInfQueryDto.java
@@ -1,2 +1,4 @@
-package activetech.edpc.pojo.dto;public class HspGrpInfQueryDto {
+package activetech.edpc.pojo.dto;
+
+public class HspGrpInfQueryDto {
 }
